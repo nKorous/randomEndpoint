96005afd3ac0964995f9e0debec07b16d206eccc
Nathaniel Korous <natework@Nathaniels-MacBook-Pro-2.local>
2018-07-11 13:16:05 -0600

    added hello world

---
+++ index.js
@@ -0,0 +1,13 @@
+let express = require('express')
+let cors = require('cors')
+let dateFormat = require('dateformat')
+let bodyParser = require('body-parser')
+
+let app = express()
+
+app.use(cors())
+app.use(bodyParser.json())
+
+app.get('hello', async (req, res) => {
+    res.status(200).send({"msg":"Hello World"})
+})
